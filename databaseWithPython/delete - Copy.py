5f8be661ff660a02023d63cdff24c24c9dc4f779
Asmit oli <82573564+Asmit34@users.noreply.github.com>
2024-04-30 17:11:45 +0545

    Delete databaseWithPython/delete - Copy.py

--- databaseWithPython/delete - Copy.py
+++
@@ -1,8 +0,0 @@
-from estd_connection import  estd_connection
-cursor=estd_connection()
-sql = """
-DELETE FROM MYSHARE
-WHERE SYMBOL = 'OHL'
-"""
-cursor.execute(sql)
-print("data deleted successfully!!")
